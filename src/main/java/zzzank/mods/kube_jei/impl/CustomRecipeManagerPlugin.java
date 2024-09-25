de4dbd0e9f2d742feb576ecfed05292d7fb2b00a
ZZZank <3410764033@qq.com>
2024-09-25 13:23:59 +0800

    safe recipe manager plugin

--- src/main/java/zzzank/mods/kube_jei/impl/CustomRecipeManagerPlugin.java
+++ src/main/java/zzzank/mods/kube_jei/impl/CustomRecipeManagerPlugin.java
@@ -1,5 +1,6 @@
 package zzzank.mods.kube_jei.impl;
 
+import dev.latvian.kubejs.util.ConsoleJS;
 import mezz.jei.api.recipe.IFocus;
 import mezz.jei.api.recipe.advanced.IRecipeManagerPlugin;
 import mezz.jei.api.recipe.category.IRecipeCategory;
@@ -7,6 +8,7 @@ import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 import zzzank.mods.kube_jei.impl.builder.CustomRecipeManagerPluginBuilder;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -26,20 +28,58 @@ public class CustomRecipeManagerPlugin implements IRecipeManagerPlugin {
 
     @Override
     public <V> @NotNull List<ResourceLocation> getRecipeCategoryUids(@NotNull IFocus<V> focus) {
-        return builder.getUidMatcher().getRecipeCategoryUids(focus.getMode(), focus.getValue());
+        if (builder.uidMatcher == null) {
+            return Collections.emptyList();
+        }
+        try {
+            return builder.uidMatcher.getRecipeCategoryUids(focus.getMode(), focus.getValue());
+        } catch (Exception e) {
+            ConsoleJS.CLIENT.error(String.format(
+                "Exception happened on providing recipe categories when focusing %s '%s'",
+                focus.getMode(),
+                focus.getValue()
+            ), e);
+            return Collections.emptyList();
+        }
     }
 
     @Override
     public <T, V> @NotNull List<T> getRecipes(
-        @NotNull IRecipeCategory<T> recipeCategory,
-        @NotNull IFocus<V> focus
+        @NotNull IRecipeCategory<T> recipeCategory, @NotNull IFocus<V> focus
     ) {
-        return builder.getRecipeMatcher().getRecipes(recipeCategory, focus.getMode(), focus.getValue());
+        if (builder.recipeMatcher == null) {
+            return Collections.emptyList();
+        }
+        try {
+            return builder.recipeMatcher.getRecipes(recipeCategory, focus.getMode(), focus.getValue());
+        } catch (Exception e) {
+            ConsoleJS.CLIENT.error(String.format(
+                "Exception happened on providing recipe for category '%s' when focusing %s '%s'",
+                recipeCategory.getUid(),
+                focus.getMode(),
+                focus.getValue()
+            ), e);
+            return Collections.emptyList();
+        }
     }
 
     @Override
     public <T> @NotNull List<T> getRecipes(@NotNull IRecipeCategory<T> recipeCategory) {
-        return builder.getNoFocusRecipeMatcher().getRecipes(recipeCategory);
+        if (builder.uidMatcher == null) {
+            return Collections.emptyList();
+        }
+        try {
+            return builder.noFocusRecipeMatcher.getRecipes(recipeCategory);
+        } catch (Exception e) {
+            ConsoleJS.CLIENT.error(
+                String.format(
+                    "Exception happened on providing recipe for category '%s'",
+                    recipeCategory.getUid()
+                ),
+                e
+            );
+            return Collections.emptyList();
+        }
     }
 
     @FunctionalInterface
