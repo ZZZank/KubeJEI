a039daab8ba727873eb227db82831827e882c107
ZZZank <3410764033@qq.com>
2024-10-13 23:58:47 +0800

    fix DualDrawable

--- src/main/java/zzzank/mods/kube_jei/util/DualDrawable.java
+++ src/main/java/zzzank/mods/kube_jei/util/DualDrawable.java
@@ -11,38 +11,39 @@ import org.jetbrains.annotations.NotNull;
  * @author ZZZank
  */
 @JSInfo("""
-    Mainly use for icons that need to display two ingredients""")
+    Mainly use for icons that want to display two ingredients""")
 @AllArgsConstructor
 public class DualDrawable implements IDrawable {
+    public static final int SIZE = 18;
+
     public final IDrawable primary;
     public final IDrawable secondary;
 
     @Override
     public int getWidth() {
-        return 18;
+        return SIZE;
     }
 
     @Override
     public int getHeight() {
-        return 18;
+        return SIZE;
     }
 
     @Override
     public void draw(@NotNull PoseStack matrixStack, int xOffset, int yOffset) {
         RenderSystem.enableDepthTest();
         matrixStack.pushPose();
-        matrixStack.translate(xOffset, yOffset, 0);
 
-        matrixStack.pushPose();
+//        matrixStack.pushPose();
         matrixStack.translate(1, 1, 0);
         primary.draw(matrixStack, xOffset, yOffset);
-        matrixStack.popPose();
+//        matrixStack.popPose();
 
-        matrixStack.pushPose();
-        matrixStack.translate(10, 10, 123);
+//        matrixStack.pushPose();
+        matrixStack.translate((SIZE + xOffset) >> 1, (SIZE + yOffset) >> 1, 100); // what
         matrixStack.scale(0.5f, 0.5f, 0.5f);
         secondary.draw(matrixStack, xOffset, yOffset);
-        matrixStack.popPose();
+//        matrixStack.popPose();
 
         matrixStack.popPose();
     }
