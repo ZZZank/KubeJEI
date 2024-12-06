ed68fa6654fafe52e961b55175a692d02db41198
ZZZank <3410764033@qq.com>
2024-12-06 16:20:35 +0800

    builder-style registration for RegisterAdvancedEventJS

--- src/main/java/zzzank/mods/kube_jei/events/register/RegisterAdvancedEventJS.java
+++ src/main/java/zzzank/mods/kube_jei/events/register/RegisterAdvancedEventJS.java
@@ -1,5 +1,6 @@
 package zzzank.mods.kube_jei.events.register;
 
+import dev.latvian.mods.rhino.annotations.typing.JSInfo;
 import lombok.val;
 import mezz.jei.api.recipe.advanced.IRecipeManagerPlugin;
 import mezz.jei.api.registration.IAdvancedRegistration;
@@ -7,10 +8,13 @@ import zzzank.mods.kube_jei.events.JEIEventJS;
 import zzzank.mods.kube_jei.impl.CustomRecipeManagerPlugin;
 import zzzank.mods.kube_jei.impl.builder.CustomRecipeManagerPluginBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class RegisterAdvancedEventJS extends JEIEventJS {
 	public final IAdvancedRegistration registration;
+	private final List<CustomRecipeManagerPluginBuilder> builders = new ArrayList<>();
 
 	public RegisterAdvancedEventJS(IAdvancedRegistration registration) {
 		this.registration = registration;
@@ -20,11 +24,31 @@ public class RegisterAdvancedEventJS extends JEIEventJS {
 		registration.addRecipeManagerPlugin(recipeManagerPlugin);
 	}
 
-	public CustomRecipeManagerPlugin createCustomRecipeManagerPlugin(Consumer<CustomRecipeManagerPluginBuilder> modifier) {
+	public CustomRecipeManagerPluginBuilder custom() {
+		val builder = new CustomRecipeManagerPluginBuilder();
+		builders.add(builder);
+		return builder;
+	}
+
+	public CustomRecipeManagerPlugin custom(Consumer<CustomRecipeManagerPluginBuilder> modifier) {
 		val builder = new CustomRecipeManagerPluginBuilder();
 		modifier.accept(builder);
 		val plugin = new CustomRecipeManagerPlugin(builder);
 		addRecipeManagerPlugin(plugin);
 		return plugin;
+	}
+
+	@JSInfo("""
+		use `custom()` instead""")
+	@Deprecated
+	public CustomRecipeManagerPlugin createCustomRecipeManagerPlugin(Consumer<CustomRecipeManagerPluginBuilder> modifier) {
+		return custom(modifier);
+	}
+
+	@Override
+	protected void afterPosted(boolean result) {
+		for (val builder : builders) {
+			addRecipeManagerPlugin(new CustomRecipeManagerPlugin(builder));
+		}
 	}
 }
