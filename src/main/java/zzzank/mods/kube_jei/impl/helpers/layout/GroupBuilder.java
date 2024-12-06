8c8e394ec3d44caf2d5d31847ec2d3b27a70b0e0
ZZZank <3410764033@qq.com>
2024-12-06 16:10:24 +0800

    helper for slot grid and ingredients applying

--- src/main/java/zzzank/mods/kube_jei/impl/helpers/layout/GroupBuilder.java
+++ src/main/java/zzzank/mods/kube_jei/impl/helpers/layout/GroupBuilder.java
@@ -6,6 +6,7 @@ import lombok.val;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.ingredient.IGuiIngredientGroup;
 import mezz.jei.api.gui.ingredient.ITooltipCallback;
+import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.IFocus;
 import zzzank.mods.kube_jei.KubeJEI;
 import zzzank.mods.kube_jei.mixins.AccessGuiIngredientGroup;
@@ -33,6 +34,47 @@ public class GroupBuilder<T, G extends IGuiIngredientGroup<T>> {
         val builder = new SlotBuilder<>(this, slots.size(), x, y);
         slots.add(builder);
         return builder;
+    }
+
+    @JSInfo("""
+        apply ingredients to added slots, as replacement of `IGuiIngredientGroup#set(IIngredients)`
+        
+        note that slots added after this action will NOT be affected, you need to manually set ingredients for them""")
+    public void applyIngredients(IIngredients ingredients) {
+        val outputs = ingredients.getOutputs(access.ingredientType()).iterator();
+        val inputs = ingredients.getInputs(access.ingredientType()).iterator();
+        for (val slot : slots) {
+            if (slot.isInput) {
+                if (inputs.hasNext()) {
+                    slot.ingredients.addAll(inputs.next());
+                }
+            } else {
+                if (outputs.hasNext()) {
+                    slot.ingredients.addAll(outputs.next());
+                }
+            }
+        }
+    }
+
+    @JSInfo("""
+        for each slot in this grid: y = yBegin + row * yStep, x = xBegin + column * xStep""")
+    public List<SlotBuilder<T>> addSlotGrid(int xBegin, int yBegin, int rows, int columns, int xStep, int yStep) {
+        val grid = new ArrayList<SlotBuilder<T>>(rows * columns);
+        for (int row = 0; row < rows; row++) {
+            val y = yBegin + row * yStep;
+            for (int col = 0; col < columns; col++) {
+                val x = xBegin + col * xStep;
+                val slot = addSlot(x, y);
+                grid.add(slot);
+            }
+        }
+        return grid;
+    }
+
+    @JSInfo("""
+        for each slot in this grid: y = yBegin + row * 18, x = xBegin + column * 18""")
+    public List<SlotBuilder<T>> addSlotGrid(int xBegin, int yBegin, int rows, int columns) {
+        return addSlotGrid(xBegin, yBegin, rows, columns, 18, 18);
     }
 
     @JSInfo("""
@@ -71,7 +113,9 @@ public class GroupBuilder<T, G extends IGuiIngredientGroup<T>> {
                 slot.xPadding,
                 slot.yPadding
             );
-            this.group.set(slot.index, slot.ingredients);
+            if (!slot.ingredients.isEmpty()) {
+                this.group.set(slot.index, slot.ingredients);
+            }
         }
     }
 }
