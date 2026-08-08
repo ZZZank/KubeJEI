5914d91e1298d65703d572ec66be52491544ff66
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-08-08 14:53:50 +0800

    register js event

--- src/main/java/zzzank/mods/kube_jei/mod_bridge/KubeJSPlugin.java
+++ src/main/java/zzzank/mods/kube_jei/mod_bridge/KubeJSPlugin.java
@@ -1,17 +1,12 @@
 package zzzank.mods.kube_jei.mod_bridge;
 
-import dev.latvian.mods.kubejs.script.BindingRegistry;
-import dev.latvian.mods.kubejs.script.TypeWrapperRegistry;
+import dev.latvian.mods.kubejs.event.EventGroupRegistry;
+import zzzank.mods.kube_jei.events.KubeJEIEvents;
 
 public class KubeJSPlugin implements dev.latvian.mods.kubejs.plugin.KubeJSPlugin {
 
 	@Override
-	public void registerBindings(BindingRegistry registry) {
-	}
-
-	@Override
-	public void registerTypeWrappers(TypeWrapperRegistry registry) {
-		// Rhino provides builtin string wrapping for enum class
-		// registry.register(RecipeIngredientRole.class, ...);
+	public void registerEvents(EventGroupRegistry registry) {
+		registry.register(KubeJEIEvents.GROUP);
 	}
 }
